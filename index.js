29f1213f59b9fb7861f9e87ee2eda6b6b43f300e
LePhasme <sebastien.courvoisier@gmail.com>
2018-11-27 16:53:56 +0100

    Add HTTP server and REST stuff to get images (with labels) and friends.

--- index.js
+++ index.js
@@ -1,12 +1,76 @@
 const fs = require('fs')
 const path = require('path')
 const puppeteer = require('puppeteer')
+const async = require('async')
+const express = require('express')
+const app = express()
+const url = require('url')
+
+app.get('/friends', function (req, res) {
+    friends(req, res)
+})
+
+app.get('/images', function (req, res) {
+    images(req, res)
+})
 
 let browser, page
 
 let user_data = JSON.parse(fs.readFileSync(path.join(__dirname, 'user_data.json')))
 
-async function run() {
+async function images(req, res) {
+    let images = []
+    if (req.query.url) {
+        await init()
+        await login()
+        if (/photos$/.test(req.query.url) === true) {
+            await page.goto(req.query.url)
+        } else {
+            await page.goto(url.resolve(req.query.url + '/', 'photos_all'))
+        }
+        await page.waitFor(3000)
+        images = await getImages()
+        await browser.close()
+        res.json(images)
+    } else {
+        await init()
+        await login()
+        await page.click('div[data-click=profile_icon]>a')
+        await page.waitForNavigation()
+        await page.goto(url.resolve(page.url() + '/', 'photos_all'))
+        await page.waitFor(3000)
+        images = await getImages()
+        await browser.close()
+        res.json(images)
+    }
+}
+
+async function friends(req, res) {
+    let friends = []
+    if (req.query.url) {
+        await init()
+        await login()
+        await page.goto(req.query.url)
+        await page.waitFor(3000)
+        await page.click('a[data-tab-key=friends]')
+        await page.waitFor(3000)
+        friends = await getFriends()
+        await browser.close()
+        res.json(friends)
+    } else {
+        await init()
+        await login()
+        await page.click('div[data-click=profile_icon]>a')
+        await page.waitFor(3000)
+        await page.click('a[data-tab-key=friends]')
+        await page.waitFor(3000)
+        friends = await getFriends()
+        await browser.close()
+        res.json(friends)
+    }
+}
+
+async function init() {
     browser = await puppeteer.launch({
         headless: false,
         args: ['--disable-notifications']
@@ -17,30 +81,51 @@ async function run() {
         height: 800,
     })
     await page.goto('https://facebook.com')
-    login()
 }
 
 async function login() {
-    await page.type('#email', user_data.email, { delay: 30 })
-    await page.type('#pass', user_data.password, { delay: 30 })
+    await page.type('#email', user_data.email, { delay: 5 })
+    await page.type('#pass', user_data.password, { delay: 5 })
     let loginButton = await page.$('#loginbutton input')
     await loginButton.click()
     await page.waitForNavigation()
-    await page.screenshot({ path: 'facebook_1.png' })
-    await page.click('div[data-click=profile_icon]>a')
-    await page.waitFor(3000)
-    await page.screenshot({ path: 'facebook_2.png' })
-    await page.click('a[data-tab-key=friends]')
-    await page.waitFor(3000)
-    let friends = await getFriends()
-    console.log(friends)
-    await browser.close()
+}
+
+async function getImages() {
+    let allImages = []
+    let allImagesSrc = []
+    for (let i = 0; i < 10; i++) {
+        let images = await page.$$eval('a.uiMediaThumb', images => images.map((a) => {
+            let href = null
+            try {
+                href = /url\(([^)]+)\);/.exec(a.innerHTML)[1].replace(/&amp;/gim, '&')
+            } catch (ignore) {}
+            return {
+                label: a.getAttribute('aria-label'),
+                page: a.href,
+                src: href
+            }
+        }))
+        images.forEach((image) => {
+            if (allImagesSrc.indexOf(image.src) === -1) {
+                allImages.push(image)
+                allImagesSrc.push(image.src)
+            }
+        })
+        await page.evaluate(_ => {
+            window.scrollBy(0, window.innerHeight)
+        })
+        await page.waitFor(1000)
+    }
+    return new Promise(resolve => {
+        resolve(allImages)
+    })
 }
 
 async function getFriends() {
     let allFriends = []
+    let allFriendsHref = []
     for (let i = 0; i < 10; i++) {
-        await page.screenshot({ path: 'facebook_' + (3 + i) + '.png' })
         let friends = await page.$$eval('div.fsl.fwb.fcb>a', friends => friends.map((a) => {
             return {
                 name: a.innerText,
@@ -48,8 +133,9 @@ async function getFriends() {
             }
         }))
         friends.forEach((friend) => {
-            if (allFriends.indexOf(friend) === -1) {
+            if (allFriendsHref.indexOf(friend.href) === -1) {
                 allFriends.push(friend)
+                allFriendsHref.push(friend.href)
             }
         })
         await page.evaluate(_ => {
@@ -62,4 +148,6 @@ async function getFriends() {
     })
 }
 
-run()
+app.listen(8090, function () {
+  console.log('Facebook scraper listening on port 8090')
+})
